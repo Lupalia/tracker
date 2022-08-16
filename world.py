a2e0275cc1c654c711bc253141f15c203537b184
Lupalia <batin.kurt.business@gmail.com>
2022-08-16 14:11:05 +0300

    three dots added, threelink folder created

--- world.py
+++ world.py
@@ -2,16 +2,23 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d
+
 
 import matplotlib.animation as animation
 
+#target point
 
 class World():
     def __init__(self):
         self.fig=plt.figure()
-        self.ax = plt.axes(projection='3d', autoscale_on=False, xlim=(-10, 10), ylim=(-10, 10))
+        self.ax = plt.axes(projection='3d', autoscale_on=False, xlim=(-10, 10), ylim=(-10, 10),zlim=(-10,10))
+        plt.plot(0,0,0,marker="o", markersize=5)
+        plt.plot(6,-5,4,marker="o", markersize=5)
+        plt.plot(-4,8,-7,marker="o", markersize=5)
         plt.show()
+    
+
+    
     def animate(self):
         pass
 
